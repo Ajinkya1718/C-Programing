59352b833e92c985942827d352c42771b69000c8
Ajinkya1718 <ajinkyashelke1000@gmail.com>
2024-05-16 19:05:05 +0530

    3rd sorting method

---
+++ Insertion Sort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,*a;
+    printf("Enter total no. of values: ");
+    scanf("%d",&n);
+    a=(int *)malloc(n*sizeof(int));
+    printf("\nEnter the values in array:-\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    
+    for(int i=1;i<n;i++)
+    {
+        int key=i;
+        int value=a[i];
+        for(int j=i-1;j>=0;j--)
+        {
+            if(a[j]>value)
+            {
+                a[j+1]=a[j];
+                key=j;
+            }
+            else
+                break;
+        }
+        a[key]=value;
+    }
+    printf("Displaying Sorted Value:-");
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+    free(a);
+}
